aac5849d3e92f917bbc9813ba0a835c1320b4175
Ahmed Istiaque <istiaquebd71@gmail.com>
2025-07-26 13:43:53 +0600

    Initial commit - ML CP

---
+++ src/main/java/com/mlcp/mlcp/MlCpApplication.java
@@ -0,0 +1,13 @@
+package com.mlcp.mlcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlCpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlCpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mlcp/mlcp/MlCpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlcp.mlcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlCpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
